2a5a40facb0e2b3262a9378fb1a99a48334b87d7
sfkaos <win.raguini@gmail.com>
2014-03-05 00:10:43 -0800

    Making pretty

--- src/com/winraguini/lyngoapp/ActionBarActivity.java
+++ src/com/winraguini/lyngoapp/ActionBarActivity.java
@@ -1,11 +1,15 @@
 package com.winraguini.lyngoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.ActionBar.Tab;
 import com.actionbarsherlock.app.ActionBar.TabListener;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
 
 public class ActionBarActivity extends SherlockFragmentActivity {
 
@@ -14,6 +18,27 @@ public class ActionBarActivity extends SherlockFragmentActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_action_bar);
 		setupTabs();
+	}
+	
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getSupportMenuInflater();
+	    inflater.inflate(R.menu.action_bar, menu);
+	    return true;
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+		  switch (item.getItemId()) {
+		    case R.id.profile_settings:
+		    	showProfileSettings();
+		      return true;
+		    default:
+		      return super.onOptionsItemSelected(item);
+		  }
+	}
+	
+	private void showProfileSettings() {
+		Intent i = new Intent(this, UserDetailsActivity.class);
+		startActivity(i);
 	}
 
 	private void setupTabs() {
